a5334559c7c8b4c603db856ae9c2d504576e142e
anowereng <anowereng@gmail.com>
2023-01-29 18:18:17 +0600

    naming convention fixed

--- S3Assesment/S3Assesment/Program.cs
+++ S3Assesment/S3Assesment/Program.cs
@@ -14,8 +14,8 @@ DateTime start = dateFromStart;
 DateTime end = dateFromEnd;
 
 double totalPoisa = 0;
-var peakhourstart = new DateTime(dateFromStart.Year, dateFromStart.Month, dateFromStart.Day, 9, 0, 0);
-var peakhourend = new DateTime(dateFromStart.Year, dateFromStart.Month, dateFromStart.Day, 22, 59, 59);
+var peakHourStart = new DateTime(dateFromStart.Year, dateFromStart.Month, dateFromStart.Day, 9, 0, 0);
+var peakHourEnd = new DateTime(dateFromStart.Year, dateFromStart.Month, dateFromStart.Day, 22, 59, 59);
 var firstOffPeakhourStart = new DateTime(dateFromStart.Year, dateFromStart.Month, dateFromStart.Day, 23, 0, 0);
 var firstOffPeakhourEnd = new DateTime(dateFromStart.Year, dateFromStart.Month, dateFromStart.Day, 23, 59, 59);
 var secondtOffPeakhourStart = new DateTime(dateFromStart.Year, dateFromStart.Month, dateFromStart.Day, 0, 0, 59);
@@ -23,7 +23,7 @@ var secondtOffPeakhourEnd = new DateTime(dateFromStart.Year, dateFromStart.Month
 while (end > start)
 {
     start = start.AddSeconds(20);
-    if (start >= peakhourstart && start <= peakhourend) totalPoisa += 30;
+    if (start >= peakHourStart && start <= peakHourEnd) totalPoisa += 30;
     if (start >= firstOffPeakhourStart && start <= firstOffPeakhourEnd) totalPoisa += 20;
     if (start >= secondtOffPeakhourStart && start <= secondtOffPeakhourEnd) totalPoisa += 20;
 }
